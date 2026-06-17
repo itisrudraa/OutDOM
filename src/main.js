e1c5e9ce1486c52361f55a5cf03c899ef74e2ef0
itisrudraa <merudra01@gmail.com>
2026-06-17 16:23:05 +0530

    add seperate container and seperate mount function

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import app from "./app.js";
 import { renderToString, render } from "./renderer.js";
 
 
-document.body.appendChild(render(app));
+function mount(container, vdom){
+    container.appendChild(render(vdom));
+};
+
+const root = document.querySelector(".root");
+
+mount(root, app);
+// document.body.appendChild(render(app));
 // document.body.innerHTML = renderToString(app);
